e1dfa469a57a0f683a90d98fcf0e1b33b94a2ff1
piumnl <piumnl@foxmail.com>
2019-10-03 08:51:03 +0800

    refactor: 将 MailUtil 的收取邮件重构，并移动到 Messager 类里

--- src/main/java/org/kotrt/maillist/core/Messager.java
+++ src/main/java/org/kotrt/maillist/core/Messager.java
@@ -1,20 +1,31 @@
 package org.kotrt.maillist.core;
 
 import java.io.IOException;
-import java.security.ProviderException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import javax.mail.Address;
+import javax.mail.Flags;
+import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Service;
 import javax.mail.Session;
+import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.MimeMessage;
+import javax.mail.search.FlagTerm;
 
 import org.kotrt.maillist.bean.User;
 import org.kotrt.maillist.core.context.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author piumnl
@@ -22,6 +33,8 @@ import org.kotrt.maillist.core.context.Context;
  * @since on 2019-09-28.
  */
 public class Messager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Messager.class);
 
     private MailProperty property;
 
@@ -32,22 +45,79 @@ public class Messager {
         this.session = session;
     }
 
-    public void sendMessage(List<? extends Message> messages) throws MessagingException {
-        Transport transport = null;
-        try {
-            // 根据 session 对象获取邮件传输对象 Transport
-            transport = session.getTransport();
-            // 设置发件人的账户名和密码
-            transport.connect(property.getUsername(), property.getPassword());
+    public List<Message> getMessages() {
+        return handle(store(), store -> {
+            try {
+                Folder folder = store.getFolder("inbox");
+                folder.open(Folder.READ_WRITE);
+                Context.getInstance().setFolder(folder);
 
-            for (Message message : messages) {
-                transport.sendMessage(buildMessage(message), getAllRecipientUser());
+                final Message[] search = folder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+                if (search == null) {
+                    return new ArrayList<>();
+                } else {
+                    return Arrays.asList(search);
+                }
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
             }
-        } catch (ProviderException e) {
+        });
+    }
+
+    private Supplier<Store> store() {
+        return () -> {
+            try {
+                return session.getStore(property.getStoreProtocol());
+            } catch (NoSuchProviderException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    /**
+     * 转发邮件
+     * @param messages 原邮件信息
+     */
+    public void sendMessage(List<? extends Message> messages) {
+        handle(transport(), service -> {
+            try {
+                LOGGER.debug("开始转发邮件：");
+                for (Message message : messages) {
+                    service.sendMessage(buildMessage(message), getAllRecipientUser());
+                    LOGGER.debug("    邮件【{}】", message.getSubject());
+                }
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+
+            // 不需要返回值
+            return null;
+        });
+    }
+
+    private Supplier<Transport> transport() {
+        return () -> {
+            try {
+                return session.getTransport();
+            } catch (NoSuchProviderException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private <T extends Service, R> R handle(Supplier<T> supplier, Function<T, R> fun) {
+        T service = null;
+        try {
+            // 获取操作对象
+            service = supplier.get();
+            // 设置发件人的账户名和密码
+            service.connect(property.getUsername(), property.getPassword());
+
+            return fun.apply(service);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
-            // 关闭邮件连接
-            close(transport);
+            close(service);
         }
     }
 
@@ -86,10 +156,10 @@ public class Messager {
                       .toArray(Address[]::new);
     }
 
-    private void close(Transport transport) {
+    private void close(Service service) {
         try {
-            if (transport != null) {
-                transport.close();
+            if (service != null) {
+                service.close();
             }
         } catch (MessagingException e) {
             throw new RuntimeException(e);
